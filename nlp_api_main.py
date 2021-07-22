11dd025d77554a8dc8157cfcdf3f0df3d212ac11
ahmed elnakeeb <ahmedelnakeeb2016@outlook.com>
2021-07-22 06:12:00 +0200

    combare feature

--- nlp_api_main.py
+++ nlp_api_main.py
@@ -4,25 +4,21 @@ import fastapi
 from pydantic import BaseModel
 from nltk.corpus import stopwords
 from gensim.models.word2vec import Word2Vec
-
-app=fastapi()
+import gensim.downloader as api
+app=FastAPI()
 
 
 @app.get("/")
 def home():
     return {"auther":"ahmedelnakeeb2016"}
 
-app.get("/compare/")
+@app.get("/compare")
 def compare_between_text(text_A:str,text_B:str):
     min_word_count = 1   # Minimum word count   
     stop_words=stopwords.words('english')
     text_A_filterd_list=[]
     text_B_filterd_list=[]
-    if(len(text_A_filterd_list)>min_word_count&len(
-        text_B_filterd_list)>min_word_count):
-        pass
-    else:
-        return{"result":"err"}
+    
     
     for i in  text_A.split():
         if i not in stop_words:
@@ -30,10 +26,9 @@ def compare_between_text(text_A:str,text_B:str):
     for i in text_B.split():
         if i not in stop_words:
             text_B_filterd_list.append(i)
-    model=Word2Vec()
-    model=model.wv.load("1.bin")
+
+    model = api.load("glove-wiki-gigaword-50")
     result=model.n_similarity(
         text_A_filterd_list,text_B_filterd_list)
     return {"result":str(result)}
-    
     
